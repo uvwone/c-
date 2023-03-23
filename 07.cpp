e51732975e232ed02cfddc0ce50571fbe3b180be
uvwone <120762631+uvwone@users.noreply.github.com>
2023-03-23 17:32:50 +0900

    Add files via upload

--- 07.cpp
+++ 07.cpp
@@ -5,15 +5,28 @@ using namespace std;
 int main()
 {
     int n;
-	scanf("%d", &n);
+    scanf("%d", &n);
 
-	for (int i = 1; i <= n; i++) {
-		if (i % 3 == 0) {
-			printf("¦\n");
-		}
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n - i; j++){
+            printf(" ");
+        }
+        for (int j = 1; j < i * 2; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
 
-		else printf("%d\n", i);
-	}
-
+    //
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++){
+            printf(" ");
+        }
+        for (int j = 0; j <= 2 *(n - i - 1); j++){
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
+
