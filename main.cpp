f12ea40275d66435f6e2e2139d84f824d9e7df7c
uvwone <120762631+uvwone@users.noreply.github.com>
2023-03-25 23:56:35 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int k, n, a1 = 0, a2 = 1;
+	scanf("%d", &n);
+	printf("%d %d ", a1, a2);
+
+	for (int i = 2; i <= n; i++) {
+		k = a1 + a2;// a1 a2 k
+		a1 = a2;
+		a2 = k;
+		printf("%d ", k);
+	}
+    return 0;
+}
